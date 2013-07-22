24188ae14b9f2e70d1af1273663a514aafc4f5f3
y_uuki <yuki.tsubo@gmail.com>
2013-07-23 03:25:06 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Frame8 - It's new $module
+
+# SYNOPSIS
+
+    use Frame8;
+
+# DESCRIPTION
+
+Frame8 is ...
+
+# LICENSE
+
+Copyright (C) y\_uuki.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+y\_uuki <yuki.tsubo@gmail.com>
---
+++ lib/Frame8.pm
@@ -0,0 +1,39 @@
+package Frame8;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Frame8 - It's new $module
+
+=head1 SYNOPSIS
+
+    use Frame8;
+
+=head1 DESCRIPTION
+
+Frame8 is ...
+
+=head1 LICENSE
+
+Copyright (C) y_uuki.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+y_uuki E<lt>yuki.tsubo@gmail.comE<gt>
+
+=cut
+
